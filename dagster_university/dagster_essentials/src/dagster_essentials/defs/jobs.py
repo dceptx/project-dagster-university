51e13af2b1ef202377de19ae0070e18b2901c8b0
David Corona <dceptx@icloud.com>
2025-07-16 17:23:21 -0600

    Finished Lesson 8 - partitions & backfills

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,15 +1,18 @@
 import dagster as dg
+from dagster_essentials.defs.partitions import monthly_partition, weekly_partition
 
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
     description="This is the trip update job. It selects all assets except trips_by_week.",
+    partitions_def=monthly_partition,
     selection=dg.AssetSelection.all() - trips_by_week,
 )
 
 weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
     description="This is the trips by week update job. It selects trips_by_week.",
+    partitions_def=weekly_partition,
     selection=trips_by_week,
 )
