23dd73df6cabaa803a9b5348346165a04e3c3b8f
David Corona <dceptx@icloud.com>
2025-07-16 15:31:21 -0600

    Finished Lesson 7 - jobs & schedules

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -4,10 +4,12 @@ trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
 trip_update_job = dg.define_asset_job(
     name="trip_update_job",
-    selection=dg.AssetSelection.all() - trips_by_week
+    description="This is the trip update job. It selects all assets except trips_by_week.",
+    selection=dg.AssetSelection.all() - trips_by_week,
 )
 
 weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
+    description="This is the trips by week update job. It selects trips_by_week.",
     selection=trips_by_week,
 )
