4bba2d80d47f93fc404c1bfafeac599fd82a5e81
David Corona <dceptx@icloud.com>
2025-07-16 13:51:21 -0600

    Finished lesson 6.

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -2,7 +2,7 @@ import os
 import duckdb
 import requests
 import dagster as dg
-from dagster._utils.backoff import backoff
+from dagster_duckdb import DuckDBResource
 from dagster_essentials.defs.assets import constants
 
 
@@ -32,7 +32,7 @@ def taxi_zones_file() -> None:
         output_file.write(raw_taxi_zones.content)
 
 @dg.asset(deps=["taxi_trips_file"])
-def taxi_trips() -> None:
+def taxi_trips(database: DuckDBResource) -> None:
     """
       The raw taxi trips dataset, loaded into a DuckDB database
     """
@@ -53,18 +53,12 @@ def taxi_trips() -> None:
         );
     """
 
-    conn = backoff(
-        fn=duckdb.connect,
-        retry_on=(RuntimeError, duckdb.IOException),
-        kwargs={
-            "database": os.getenv("DUCKDB_DATABASE"),
-        },
-        max_retries=10,
-    )
-    conn.execute(query)
+    with database.get_connection() as conn:
+        conn.execute(query)
+
 
 @dg.asset(deps=["taxi_zones_file"])
-def taxi_zones() -> None:
+def taxi_zones(database: DuckDBResource) -> None:
     """
       The raw taxi zones dataset, loaded into a DuckDB database
     """
@@ -78,14 +72,5 @@ def taxi_zones() -> None:
             from '{constants.TAXI_ZONES_FILE_PATH}'
         );
     """
-
-    conn = backoff(
-        fn=duckdb.connect,
-        retry_on=(RuntimeError, duckdb.IOException),
-        kwargs={
-            "database": os.getenv("DUCKDB_DATABASE"),
-        },
-        max_retries=10,
-    )
-
-    conn.execute(query)
+    with database.get_connection() as conn:
+        conn.execute(query)
